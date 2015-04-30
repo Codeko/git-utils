c54305c86b996517d631d8cdd5783edeef810d97
Boris Burgos <bburgos@codeko.com>
2015-04-30 11:43:07 +0200

    Common funcions

---
+++ bin/gitutil_common.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# -*- ENCODING: UTF-8 -*-
+
+#Stops script if any error
+set -e
+
+CURRENT_PATH=`pwd`
+
+
+function check_git_dir(){
+	if [ ! -d .git ]; then
+            echo "Can't find git in the current path: $CURRENT_PATH"
+            exit -1
+        fi
+}
+
