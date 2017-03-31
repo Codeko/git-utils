6a0fbfde63c929266d7ea5e0be1f453d442b6c36
oscar <oscar@oscar-H81M-S2H>
2017-03-31 13:49:26 +0200

    Modificacion

--- install.js
+++ install.js
@@ -1,14 +1,12 @@
 const utils = require('./utils');
 const execFile = require('child_process').execFile;
-const homedir = require('homedir');
-const fs = require('fs-extra');
 const path = require('path');
 
 var script = path.resolve(__dirname) + '/';
-script += 'install.sh';
+script = 'install.sh';
 execFile(script, function(err, out, stderr) {
-	if (err instanceof Error)
-	  throw err;
+	if (err !== null)
+	  console.log(err);
 	  process.stderr.write(stderr);
 	  process.stdout.write(out); 
 	});
