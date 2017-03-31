945fa7faad715d9d31b0bea9123a60594738ed84
oscar <oscar@oscar-H81M-S2H>
2017-03-31 11:39:21 +0200

    Añadido Composer 8

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-sudo apt-get install realpath
+echo "source ~/bin/git-changesarchive" >>~/bin/changesarchive
+echo "source ~/bin/git-diffarchive" >>~/bin/git-diffarchive
+echo "source ~/bin/git-pullall" >>~/bin/git-pullall
+echo "source ~/bin/git-pushall" >>~/bin/git-pushall
+echo "source ~/bin/git-updateall" >>~/bin/git-updateall
+echo "source ~/bin/gitutil_common.sh" >>~/bin/gitutil_common.sh
