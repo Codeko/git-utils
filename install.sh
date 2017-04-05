ba12e315c2839a77048e445dd1df86961f2776e2
oscar <oscar@oscar-H81M-S2H>
2017-04-05 10:20:10 +0200

    Modificaciones 2

--- install.sh
+++ install.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 echo "chmod 777 /usr/local/lib/node_modules/git-utils"
-echo "source ~/git-utils/bin/git-changesarchive" >>~/git-utils/bin/git-changesarchive
-echo "source ~/git-utils/bin/git-diffarchive" >>~/git-utils/bin/git-diffarchive
-echo "source ~/git-utils/bin/git-pullall" >>~/git-utils/bin/git-pullall
-echo "source ~/git-utils//bin/git-pushall" >>~/git-utils/bin/git-pushall
-echo "source ~/git-utils/bin/git-updateall" >>~/git-utils/bin/git-updateall
-echo "source ~/git-utils/bin/gitutil_common.sh" >>~/git-utils/bin/gitutil_common.sh
 echo "chmod 777 ~/git-utils/bin/*"
-sudo mv ./node_modules/git-utils ./node_modules/..
+sudo mv ./node_modules/git-utils .
