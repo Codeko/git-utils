9b0afe9aa7e574823dbe87b2707ba4fcd07f10b8
oscar <oscar@oscar-H81M-S2H>
2017-04-05 11:21:33 +0200

    Modificaciones 4

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 echo "chmod 777 /usr/local/lib/node_modules/git-utils"
-cp $HOME/.npm/git-utils/1.0.0/package.tgz .
-tar -xvzf package.tgz
+cp -R node_modules/git-utils/* .
 echo "chmod 777 ~/git-utils/bin/*"
