fd134ac306fdd4dfcfa0c743f094ffc7007b77ac
oscar <oscar@oscar-H81M-S2H>
2017-04-06 10:32:19 +0200

    Prueba autocompletado 1

--- install.sh
+++ install.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-sudo mv bin/git-completion.bash /etc/bash_completion.d
-let var=$(grep "source /etc/bash_completion.d/git-completion.bash" ~/.bashrc|wc -l)
-let comp=0;
-if test $var -eq $comp
-then
-   echo "source /etc/bash_completion.d/git-completion.bash">> ~/.bashrc
-fi
 cd ..
 sudo mv git-utils ..
